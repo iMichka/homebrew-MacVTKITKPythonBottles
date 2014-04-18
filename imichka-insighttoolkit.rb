79c487dc5924dbbbfe17245c377b84100ee006b9
Michka Popoff <michkapopoff@gmail.com>
2014-04-19 00:02:27 +0200

    Update to 4.5.2

--- imichka-insighttoolkit.rb
+++ imichka-insighttoolkit.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class ImichkaInsighttoolkit < Formula
   homepage 'http://www.itk.org'
-  url 'http://downloads.sourceforge.net/project/itk/itk/4.5/InsightToolkit-4.5.1.tar.gz'
-  sha1 'f228b39783f3490e861006b1d2ad18a5f4d4522d'
+  url 'http://downloads.sourceforge.net/project/itk/itk/4.5/InsightToolkit-4.5.2.tar.gz'
+  sha1 '91b14d4a67e837b3e0dc12d2d7ad5b3fdaae5a8e'
   head 'git://itk.org/ITK.git'
-  
+
   bottle do
     root_url 'http://download.sf.net/project/macvtkitkpythonbottles/itk'
-    revision 2
-    sha1 'cd53f0eff716a7ceeb21a7951a831cb73510bd22' => :mavericks
-    sha1 'cd53f0eff716a7ceeb21a7951a831cb73510bd22' => :mountain_lion
-    sha1 '159cc059ae253fdb95d49726fcd9902d4cf84bd2' => :lion
+    revision 1
+    sha1 'c24926a3691edb12ff4e103e49a6f76a316314de' => :mavericks
+    sha1 'c24926a3691edb12ff4e103e49a6f76a316314de' => :mountain_lion
+    sha1 '2ff126aa2a7179675aa8bde275db652710b9679d' => :lion
   end
 
 
@@ -34,7 +34,7 @@ class ImichkaInsighttoolkit < Formula
   option 'with-review', 'Enable modules under review'
 
   def install
-    
+
     args = std_cmake_args + %W[
       -DBUILD_TESTING=OFF
       -DBUILD_SHARED_LIBS=ON
@@ -68,19 +68,19 @@ class ImichkaInsighttoolkit < Formula
           -DCMAKE_C_FLAGS='-ansi'
         ]
         # CMake picks up the system's python dylib, even if we have a brewed one.
-        args << "-DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.6/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib"
+        args << "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'"
       end
       system "cmake", *args
       system "make", "install"
     end
   end
-  
+
   def post_install
     # Put manually the pth file in the site-packages folder
     # Waiting for modification of the ITK install script to have the following structure :
     # /Cellar/imichka-insighttoolkit/4.5.1/lib/python/site-packages
     if pour_bottle? and Formula.factory('python').installed?
-      File.open("#{HOMEBREW_PREFIX}/lib/python2.7/site-packages/WrapITK.pth", 'w') {|f| f.write("#{HOMEBREW_PREFIX}/Cellar/imichka-insighttoolkit/4.5.1/lib/ITK-4.5/Python") }
+      File.open("#{HOMEBREW_PREFIX}/lib/python2.7/site-packages/WrapITK.pth", 'w') {|f| f.write("#{HOMEBREW_PREFIX}/Cellar/imichka-insighttoolkit/4.5.2/lib/ITK-4.5/Python") }
     end
   end
 
